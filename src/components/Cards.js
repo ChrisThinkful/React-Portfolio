6964d38839de4650ead88c2777ce26ce086c7fac
Chris <78775458+ChrisThinkful@users.noreply.github.com>
2021-11-18 17:57:13 -0600

    Update Cards.js
    Replaced bootcamp projects with newer projects

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,46 +5,46 @@ import './Cards.css';
 function Card() {
     return (
         <div className='cards'>
-            <h2>LATEST PROJECTS</h2>
+            <h2>PROJECTS</h2>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem 
-                        src='images/reservation.jpg'
-                        text='A web application used by restaurant personnel to create, read, update, and delete reservations for customers.'
-                        altText='Reservations App'
-                        label='Periodic Tables'
-                        path='https://github.com/ChrisThinkful/Restaurant-Reservations'
+                        src=''
+                        text='A library application created using HTML, CSS, and JavaScript.'
+                        altText='Library App'
+                        label='Track Your Books'
+                        path='https://github.com/ChrisThinkful/library'
                         />
                         <CardItem 
-                        src='images/movie.jpg' 
-                        text='Check out reviews of your favorite movies or write a review of one you have just watched.'
-                        altText='Movies App'
-                        label='WeLoveMovies'
-                        path='https://github.com/ChrisThinkful/Project_WeLoveMovies_1'
+                        src='' 
+                        text='Do some quick maths with this Calculation'
+                        altText='Calculator App'
+                        label='Calculator'
+                        path='https://github.com/ChrisThinkful/Calculator'
                         />
                     </ul>
                     <ul className="cards__items">
                         <CardItem 
-                        src='images/food.jpg' 
-                        text='Order and have food from your favorite restaurants delivered to your door!'
-                        altText='Food Delivery'
-                        label='GrubDash'
-                        path='https://github.com/ChrisThinkful/Project_GrubDash_Qualified_1'
+                        src='' 
+                        text='Travel back in time with this web-based recreation of a childhood favorite - The Etch-A-Sketch!'
+                        altText='Etch-A-Sketch'
+                        label='Etch-A-Sketch'
+                        path='https://github.com/ChrisThinkful/Etch-A-Sketch'
                         />
                         <CardItem 
-                        src='images/flashcards.jpg' 
-                        text='Flashcards are a proven methodology for learning. Now, you can create cards and decks right in this application!'
-                        altText='Flashcards'
-                        label='Flashcard-O-Matic'
-                        path='https://github.com/ChrisThinkful/Project_Flashcards_Qualified_1'
+                        src='' 
+                        text="Have a go at this nerd's take on Rock Paper Scissors"
+                        altText='Rock Paper Scissors'
+                        label='Warrior Wizard Ranger'
+                        path='https://github.com/ChrisThinkful/Rock-Paper-Scissors'
                         />
                         <CardItem 
-                        src='images/timer.jpg'
-                        text='Cycle between periods of Focus and Break using the Pomodoro Timer.'
-                        altText='Tomato Timer'
-                        label='Pomodoro Study Timer'
-                        path='https://github.com/ChrisThinkful/Pomodoro_Timer'
+                        src=''
+                        text='A sample landing page for a portfolio or blog.'
+                        altText='Landing Page'
+                        label='Awesome Landing Page'
+                        path='https://christhinkful.github.io/Landing-Page/'
                         />
                     </ul>
                 </div>
