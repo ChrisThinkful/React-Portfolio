b333692a343c6c893c6c1d9e1aa2fceac51e95c8
Chris <78775458+ChrisThinkful@users.noreply.github.com>
2021-11-19 10:56:40 -0600

    Project gif

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,7 +10,7 @@ function Card() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem 
-                        src=''
+                        src='public/images/Library.gif'
                         text='A library application created using HTML, CSS, and Javascript.'
                         altText='Library App'
                         label='Track Your Books'
